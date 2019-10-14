e129148f4c75f43426f10553548e7ca33a8f7c90
Marco Bruno <marco.bruno.br@gmail.com>
2019-10-14 02:56:35 +0100

    :sparkles: add linter, prettier, husky, wiston and morgan

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
-import restify from "restify";
-import dotenv from "dotenv";
+import restify from 'restify';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import winston from 'winston';
+import logger from './config/winston';
 
 dotenv.config();
 
 const app = restify.createServer();
 const { PORT } = process.env;
 
-app.get("/", (req, res) => res.send({ hello: "world" }));
+app.use(morgan('combined', { stream: winston.stream }));
+app.get('/', (req, res) => res.send({ hello: 'world' }));
 
 app.listen(PORT, () => {
-  console.log(`Listening at http://localhost:${PORT}`);
-  console.log("Turn off server: ctrl + c");
+  logger.silly();
+  logger.info(`Listening at http://localhost:${PORT}`);
+  logger.info('Turn off server: ctrl + c');
 });
