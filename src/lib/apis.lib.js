6b79c61fb560912cb1d1f662e9cb22068ac82536
Marco Bruno <marco.bruno.br@gmail.com>
2020-03-01 09:25:05 +0100

    :zap: Change environment variable to the email API

--- src/lib/apis.lib.js
+++ src/lib/apis.lib.js
@@ -2,10 +2,10 @@ import { config } from 'dotenv';
 import superagent from 'superagent';
 
 config();
-const { HOST, PORT, API_EMAIL } = process.env;
+const { HOST, PORT, HOST_API_EMAIL } = process.env;
 
 async function sendUserConfirmationEmail(email, token) {
-  const url = `${API_EMAIL}/user/confirmation`;
+  const url = `${HOST_API_EMAIL}/user/confirmation`;
   const contentLink = `${HOST}:${PORT}/user/confirmation`;
   const link = `${contentLink}/${token}`;
   const res = await superagent.post(url).send({
